0a31fded7a0d9bb08f077351bda318cba89a0c60
useifabdelhady <yoazazy55@gmail.com>
2024-09-07 17:12:27 +0300

    Add project files.

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = "Server=.;Database=DVLD;User Id=sa;Password=sa123456;";
+
+
+    }
+}
